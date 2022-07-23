0f5928ef4182958a30bbaa2c1be841266878891c
ferfox88 <ferfox@yandex.ru>
2022-07-23 14:46:31 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.join(''));
+  console.log(bracketsConfig);
+  while (str.length > 0) {
+    let delLength = str.length;
+    for (const bracket of bracketsConfig) {
+      if (str.indexOf(bracket) >= 0) {
+        str = str.replace(bracket, '');
+      }
+    }
+    if (delLength === str.length) {
+      return false;
+    }
+  }
+  return true;
 }
